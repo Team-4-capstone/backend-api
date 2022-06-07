2e0b7cd3340a1912d7e6691423f50e656c52bc59
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-07 15:44:06 -0500

    implementing categories relationshipd with reports table

--- src/main/java/com/codeup/backendapi/data/Category.java
+++ src/main/java/com/codeup/backendapi/data/Category.java
@@ -1,4 +1,31 @@
 package com.codeup.backendapi.data;
 
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@DynamicUpdate
 public class Category {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "uxo_description",
+            joinColumns =
+            @JoinColumn(name = "reports_id"),
+            inverseJoinColumns =
+            @JoinColumn(name = "uxo_description"))
+
+    private List<Report> report;
+
+
+
 }
