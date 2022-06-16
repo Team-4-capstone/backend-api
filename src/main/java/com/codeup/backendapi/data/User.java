665ab25f92616cd2c16614aa203e71dc05f51aea
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-06-16 16:37:08 -0500

    added seeder for join tables

--- src/main/java/com/codeup/backendapi/data/User.java
+++ src/main/java/com/codeup/backendapi/data/User.java
@@ -1,20 +1,20 @@
-package com.codeup.backendapi.data;
-
-import javax.persistence.Entity;
-import java.time.LocalDateTime;
-
-public class User {
-    @Entity
-
-    private long id;
-    private String username;
-    private String email;
-    private String password;
-    private LocalDateTime createdAt;
-    private Role role;
-
-
-    public enum Role {USER, ADMIN};
-
-
-}
+//package com.codeup.backendapi.data;
+//
+//import javax.persistence.Entity;
+//import java.time.LocalDateTime;
+//
+//public class User {
+//    @Entity
+//
+//    private long id;
+//    private String username;
+//    private String email;
+//    private String password;
+//    private LocalDateTime createdAt;
+//    private Role role;
+//
+//
+//    public enum Role {USER, ADMIN};
+//
+//
+//}
--- src/main/resources/scripts/db_create.sql
+++ src/main/resources/scripts/db_create.sql
@@ -2,16 +2,16 @@ DROP DATABASE if exists bomb_reporter_db;
 CREATE DATABASE if not exists bomb_reporter_db;
 USE bomb_reporter_db;
 
-CREATE TABLE users
-(
-    id         BIGINT       NOT NULL AUTO_INCREMENT,
-    username   VARCHAR(60)  NOT NULL,
-    password   VARCHAR(60)  NOT NULL,
-    email      VARCHAR(200) NOT NULL,
-    role       VARCHAR(32)  NOT NULL,
-    created_at DATETIME     NOT NULL,
-    primary key (id)
-);
+# CREATE TABLE users
+# (
+#     id         BIGINT       NOT NULL AUTO_INCREMENT,
+#     username   VARCHAR(60)  NOT NULL,
+#     password   VARCHAR(60)  NOT NULL,
+#     email      VARCHAR(200) NOT NULL,
+#     role       VARCHAR(32)  NOT NULL,
+#     created_at DATETIME     NOT NULL,
+#     primary key (id)
+# );
 
 CREATE TABLE reports
 (
