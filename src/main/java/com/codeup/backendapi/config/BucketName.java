f56150f5054429a4c6a7f7308fef3cda9e116825
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-22 18:15:03 -0400

    comment out aws bucket

--- src/main/java/com/codeup/backendapi/config/BucketName.java
+++ src/main/java/com/codeup/backendapi/config/BucketName.java
@@ -1,14 +1,14 @@
-package com.codeup.backendapi.config;
-
-public enum BucketName {
-    TODO_IMAGE("oceans");
-    private final String bucketName;
-
-    BucketName(String bucketName) {
-        this.bucketName = bucketName;
-    }
-
-    public String getBucketName() {
-        return bucketName;
-    }
-}
+//package com.codeup.backendapi.config;
+//
+//public enum BucketName {
+//    TODO_IMAGE("oceans");
+//    private final String bucketName;
+//
+//    BucketName(String bucketName) {
+//        this.bucketName = bucketName;
+//    }
+//
+//    public String getBucketName() {
+//        return bucketName;
+//    }
+//}
