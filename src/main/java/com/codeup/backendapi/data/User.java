429f570f62cea129a682cb6137bfec58733e257c
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-06-22 18:12:25 -0400

    add user and repo

--- src/main/java/com/codeup/backendapi/data/User.java
+++ src/main/java/com/codeup/backendapi/data/User.java
@@ -1,20 +1,63 @@
-//package com.codeup.backendapi.data;
-//
-//import javax.persistence.Entity;
-//import java.time.LocalDateTime;
-//
-//public class User {
-//    @Entity
-//
-//    private long id;
-//    private String username;
-//    private String email;
-//    private String password;
-//    private LocalDateTime createdAt;
-//    private Role role;
-//
-//
-//    public enum Role {USER, ADMIN};
-//
-//
-//}
+package com.codeup.backendapi.data;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table (name = "users")
+@DynamicUpdate
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+    private Boolean locked;
+    private Boolean enabled;
+    //    private LocalDateTime createdAt;
+
+
+    public User(String name, String username, String email, String password, Role role, Boolean locked, Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public enum Role {USER, ADMIN};
+}
+
+
